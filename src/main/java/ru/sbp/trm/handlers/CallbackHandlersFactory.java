1174e47dfae38f5c73673d8b35b9bb1995c3d257
SbpTrm <managertestresource@gmail.com>
2021-07-08 21:49:48 +0300

    add bot logic

--- src/main/java/ru/sbp/trm/handlers/CallbackHandlersFactory.java
+++ src/main/java/ru/sbp/trm/handlers/CallbackHandlersFactory.java
@@ -1,6 +1,7 @@
 package ru.sbp.trm.handlers;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.sbp.trm.core.BotActions;
 
@@ -8,18 +9,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class CallbackHandlersFactory {
     public static final String CALLBACK_DATA_SPLITTER = "|";
     private static final Map<String, Class<? extends CallbackHandler>> CALLBACK_HANDLERS_CLASSES = new HashMap<>();
 
     static {
         CALLBACK_HANDLERS_CLASSES.put(BotActions.SHOW_MAIN_MENU.toString(), MainMenuHandler.class);
+        CALLBACK_HANDLERS_CLASSES.put(BotActions.SHOW_FREE.toString(), ShowFreeResourcesHandler.class);
     }
 
     @SneakyThrows
     public CallbackHandler getCallbackHandler(String callbackData) {
         String actionLabel = callbackData.split(CALLBACK_DATA_SPLITTER)[0];
-        Class<? extends CallbackHandler> aClass = CALLBACK_HANDLERS_CLASSES.get(actionLabel);
-        return aClass.getConstructor().newInstance();
+        log.info("Getting handler for label {}", actionLabel);
+        Class<? extends CallbackHandler> handlerClass = CALLBACK_HANDLERS_CLASSES.get(actionLabel);
+        log.info("Using class {} for label {}", handlerClass, actionLabel);
+        return handlerClass.getConstructor().newInstance();
     }
 }
