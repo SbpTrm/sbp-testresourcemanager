4f8f99dcd94deb7d5dc98033c6d24295446d75a0
SbpTrm <managertestresource@gmail.com>
2021-12-04 12:08:52 +0300

    move to kotlin

--- src/main/kotlin/trm/core/MessageFactory.kt
+++ src/main/kotlin/trm/core/MessageFactory.kt
@@ -6,7 +6,13 @@ import org.telegram.telegrambots.meta.api.objects.CallbackQuery
 import org.telegram.telegrambots.meta.api.objects.Update
 
 fun Update.userId() = this.callbackQuery.from.id.toString()
-fun Update.chatId() = this.callbackQuery.message.chat.id.toString()
+fun Update.chatId(): String {
+    return if (this.callbackQuery != null) {
+        this.callbackQuery.message.chat.id.toString()
+    } else {
+        this.message.chat.id.toString()
+    }
+}
 
 fun Update.createMyResourcesMessage(): BotApiMethod<*> = SendMessage.builder()
     .chatId(this.chatId())
