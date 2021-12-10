1c1beee63f7d0aa1b81c59d30ff55bc906de7f71
SbpTrm <managertestresource@gmail.com>
2021-12-10 21:39:28 +0300

    add db

--- src/main/kotlin/trm/config/TestResourceManagerConfigurationProperties.kt
+++ src/main/kotlin/trm/config/TestResourceManagerConfigurationProperties.kt
@@ -1,7 +1,5 @@
 package trm.config
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 
