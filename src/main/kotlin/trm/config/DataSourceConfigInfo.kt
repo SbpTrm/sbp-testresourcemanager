836fa3e0c9b133ba93318f5c0c36027894edf24b
SbpTrm <managertestresource@gmail.com>
2021-12-11 22:24:01 +0300

    add db

--- src/main/kotlin/trm/config/DataSourceConfigInfo.kt
+++ src/main/kotlin/trm/config/DataSourceConfigInfo.kt
@@ -1,22 +1,20 @@
 package trm.config
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.actuate.info.Info
-import org.springframework.boot.actuate.info.InfoContributor
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
 
-@Component
-class DataSourceConfigInfo : InfoContributor {
-    @Autowired
-    lateinit var dataSourceProperties: DataSourceProperties
+@Configuration
+open class DataSourceConfigInfo(@Autowired val dataSourceProperties: DataSourceProperties) {
 
-    override fun contribute(builder: Info.Builder) {
-        builder.withDetail(
-            "dataSourceProperties", mapOf(
-                "url" to dataSourceProperties.url,
-                "driver" to dataSourceProperties.driverClassName
-            )
-        )
+    @Bean
+    open fun dataSource(): DataSource? {
+        val config = HikariConfig()
+        config.jdbcUrl = dataSourceProperties.url
+        return HikariDataSource(config)
     }
 }
