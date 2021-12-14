a16a39b006a4d08c09f5144717ee408ad4a8c5ce
SbpTrm <managertestresource@gmail.com>
2021-12-14 21:13:24 +0300

    add db

--- src/main/kotlin/trm/controller/TestResourceController.kt
+++ src/main/kotlin/trm/controller/TestResourceController.kt
@@ -2,11 +2,13 @@ package trm.controller
 
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod
 import org.telegram.telegrambots.meta.api.objects.Update
 import trm.TestResourceManagerBot
 
-class TestResourceController(private val testResourceManagerBot: TestResourceManagerBot) {
+@RestController
+open class TestResourceController(private val testResourceManagerBot: TestResourceManagerBot) {
     @PostMapping(path = ["/"])
     fun onUpdateReceived(@RequestBody update: Update): BotApiMethod<*> =
         testResourceManagerBot.onWebhookUpdateReceived(update)
